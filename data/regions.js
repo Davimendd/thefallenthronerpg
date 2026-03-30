568388c3865ff18cb5812cbad4f96138b90efb8f
davimd <davimendesteia@gmail.com>
2026-03-30 01:26:17 -0300

    Add files via upload

--- data/regions.js
+++ data/regions.js
@@ -7,49 +7,49 @@ As grandes terras que moldam o destino dos Sete Reinos. Cada região possui cult
 <div class="regions-grid">
 
   <div class="region-card">
-    <img src="https://i.imgur.com/4K7Xn0B.jpg" alt="O Norte">
+    <img src="https://awoiaf.westeros.org/images/thumb/6/66/T_Jedruszek_NorthernInfantry.jpg/350px-T_Jedruszek_NorthernInfantry.jpg" alt="O Norte">
     <h3>O Norte</h3>
     <p>Terras frias e vastas, governadas pela Casa Stark. Honra e resistência definem seu povo.</p>
   </div>
 
   <div class="region-card">
-    <img src="https://i.imgur.com/U4sZqfQ.jpg" alt="Terras Fluviais">
+    <img src="https://upload.gtarcade.com/got/2019/05/155894006162324513.jpg" alt="Terras Fluviais">
     <h3>Terras Fluviais</h3>
     <p>Rios, castelos e guerras constantes. Região disputada por diversas casas.</p>
   </div>
 
   <div class="region-card">
-    <img src="https://i.imgur.com/3sZk6pP.jpg" alt="O Vale">
+    <img src="https://static.wikia.nocookie.net/gameofthrones/images/f/ff/Ninho.jpg/revision/latest?cb=20161206220518&path-prefix=pt-br" alt="O Vale">
     <h3>O Vale</h3>
     <p>Montanhas impenetráveis e cavaleiros orgulhosos protegidos pelo Ninho da Águia.</p>
   </div>
 
   <div class="region-card">
-    <img src="https://i.imgur.com/w7U9hQK.jpg" alt="Terras Ocidentais">
+    <img src="https://gotrpg.weebly.com/uploads/1/2/6/1/126151493/ocidente_orig.jpg" alt="Terras Ocidentais">
     <h3>Terras Ocidentais</h3>
     <p>Ricas em ouro e poder, sob domínio da Casa Lannister.</p>
   </div>
 
   <div class="region-card">
-    <img src="https://i.imgur.com/1H8GJdP.jpg" alt="Terras da Coroa">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCY0nvea_FtATaYwqdOscOTj6TmifD5INpkg&s" alt="Terras da Coroa">
     <h3>Terras da Coroa</h3>
     <p>Centro político dos Sete Reinos, onde fica Porto Real e o Trono de Ferro.</p>
   </div>
 
   <div class="region-card">
-    <img src="https://i.imgur.com/W1ZtC0b.jpg" alt="Campina">
+    <img src="https://static.wikia.nocookie.net/sss/images/7/7d/T_Jedruszek_wardensofthereach.jpg/revision/latest?cb=20170822065350&path-prefix=pt-br" alt="Campina">
     <h3>A Campina</h3>
     <p>Campos férteis, cavaleiros e riqueza agrícola. Governada pela Casa Tyrell.</p>
   </div>
 
   <div class="region-card">
-    <img src="https://i.imgur.com/J9G9FZK.jpg" alt="Dorne">
+    <img src="https://preview.redd.it/what-your-thoughts-about-dorne-v0-d50cjzysapsd1.jpeg?auto=webp&s=f55d4f3b8782a869717018ee4fe22d57c34caea2" alt="Dorne">
     <h3>Dorne</h3>
     <p>Terras quentes e independentes, com cultura e costumes únicos.</p>
   </div>
 
   <div class="region-card">
-    <img src="https://i.imgur.com/Q7p1f0K.jpg" alt="Ilhas de Ferro">
+    <img src="https://preview.redd.it/i-made-the-iron-islands-from-game-of-thrones-in-far-cry-5-v0-t1rbt9smfw581.jpg?auto=webp&s=18dbc3cb1d0f3765bc44b1a25191d947780c0061" alt="Ilhas de Ferro">
     <h3>Ilhas de Ferro</h3>
     <p>Lar de saqueadores e navegadores ferozes que seguem o Deus Afogado.</p>
   </div>
